40650be684d6a603a8aafda165460bb7c1981442
minjoo <joozzoo0223@gmail.com>
2022-07-23 14:42:53 +0900

    feat:고객이용약관

---
+++ src/pages/term/userTerm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import { PageWrap } from "../../shared/styled";
+
+import term_data from '../../data/userTerm.json';
+
+const UserTerm = () => {
+
+  return (
+    <PageWrap>
+      <TermWrap>
+        <div className="title">서비스 이용약관</div>
+        <div className="detail">{term_data.terms[0].userTerm}</div>
+      </TermWrap>
+    </PageWrap>
+  );
+};
+
+const TermWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1.875rem 1.75rem;
+  white-space: pre-wrap;
+  font-family: "Apple SD Gothic Neo";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+  color: #ffffff;
+
+  .title{
+    margin-bottom: 2.3125rem;
+  }
+
+  .detail{
+    font-size: 0.75rem;
+  }
+`;
+export default UserTerm;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/landing/main";
 import Terms from "../pages/term/terms";
+import UserTerm from "../pages/term/userTerm";
 
 const RootRoute = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Terms />} />
+        <Route path="/" element={<Main />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/terms/user" element={<UserTerm />} />
       </Routes>
     </BrowserRouter>
   );
